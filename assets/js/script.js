b046c288510d68d0c51368fcd13b82b59798993c
David Manriquez <dmanriqu@my.chemeketa.edu>
2021-11-28 22:23:02 -0800

    assingment code + event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// event listener to generate button/ assignment Code
+document.querySelector("#generate");
+addEventListener("click", writePassword);
 
 //create character banks for numbers letters and special characters
 var specialCharacters = ["!","#","$","%","^","&","*","(",")","`","=","-","_","+","\:","<",">","?","}","?","{"];
@@ -11,11 +12,11 @@ var lettersUpper = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", 
 var lettersUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 
-//when user clicks generate password button, prompt user for their character selections
+//when user clicks generate password button prompt user for length of password
 function generatePassword (){
     var confirmLength = (prompt("How many characters would you like the password to have?"));
 }
-//Loop in case answer does not fit criteria 
+//while loop in case answer does not fit criteria 
 while(confirmLength<= 7 || confirmLength >= 128){
     alert("Password length must be between 8-128 characters");
 var confirmLength = (prompt("How many characters would you like you password to contain"));
@@ -24,13 +25,13 @@ var confirmLength = (prompt("How many characters would you like you password to 
 //display length of random characters corresponding to user length parameter
 alert(`Your password will have ${confirmLength}characters`);
 
-//store character selections for use later
+//determine what parameters are for use later
+var confirmSpecialCharacter = confirm("Click ok if you would like to use special characters")
+
 //randomly select characters from user selections for password and store them
-//prompt user for length of password
 
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
