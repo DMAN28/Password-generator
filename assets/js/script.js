6eb98d7b6683fa40d39c64dcaeb519615acbde08
David Manriquez <dmanriqu@my.chemeketa.edu>
2021-11-28 21:31:36 -0800

    character bank

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
+//create character banks for numbers letters and special characters
+var specialCharacters = ["!","#","$","%","^","&","*","(",")","`","=","-","_","+","\:","<",">","?","}","?","{"];
+// Numeric characters
+var number = ['1', '2', '3', '4', '5', '6', '7', '8', '9',];
+// Alphabetical characters
+var lettersUpper = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var lettersUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+
+//when user clicks generate password button, prompt user for their character selections
+//store character selections for use later
+//randomly select characters from user selections for password and store them
+//prompt user for length of password
+//display length of random characters corresponding to user length parameter
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -12,3 +28,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
