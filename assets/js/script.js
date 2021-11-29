30fb32e8f535a764aa8b8ae36f777b3415b3397c
David Manriquez <dmanriqu@my.chemeketa.edu>
2021-11-28 21:52:44 -0800

    character length prompt

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,23 @@
 //create character banks for numbers letters and special characters
 var specialCharacters = ["!","#","$","%","^","&","*","(",")","`","=","-","_","+","\:","<",">","?","}","?","{"];
-// Numeric characters
-var number = ['1', '2', '3', '4', '5', '6', '7', '8', '9',];
-// Alphabetical characters
+// numeric characters
+var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9',];
+// alphabetical characters
 var lettersUpper = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 var lettersUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 
 //when user clicks generate password button, prompt user for their character selections
+function generatePassword (){
+    var confirmLength = (prompt("How many characters would you like the password to have?"));
+}
+//Loop in case answer does not fit criteria 
+while(confirmLength<= 7 || confirmLength >= 128){
+    alert("Password length must be between 8-128 characters");
+var confirmLength = (prompt("How many characters would you like you password to contain"));
+}
+
 //store character selections for use later
 //randomly select characters from user selections for password and store them
 //prompt user for length of password
